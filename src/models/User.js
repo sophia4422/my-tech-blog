6e54c281b7d575518bdd9e1255bd66c2ead3ac54
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-07-02 22:08:53 +0100

    password hashing setup

--- src/models/User.js
+++ src/models/User.js
@@ -1,28 +1,54 @@
+const bcrypt = require("bcrypt");
 const { Model, DataTypes } = require("sequelize");
 
 const connection = require("../config/connection");
+const { hashPassword } = require("../hooks");
 
-class User extends Model {}
+class User extends Model {
+  async checkPassword(password) {
+    const isValid = await bcrypt.compare(password, this.password);
+    return isValid;
+  }
+}
 
 const schema = {
   id: {
     type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
   },
   firstName: {
     type: DataTypes.STRING,
+    allowNull: false,
   },
   lastName: {
     type: DataTypes.STRING,
+    allowNull: false,
   },
   email: {
     type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    },
   },
   password: {
     type: DataTypes.STRING,
+    allowNull: false,
   },
 };
 
-const options = {};
+const options = {
+  sequelize: connection,
+  timestamps: true,
+  underscored: false,
+  freezeTableName: true,
+  modelName: "user",
+  hooks: {
+    beforeCreate: hashPassword,
+  },
+};
 
 User.init(schema, options);
 
