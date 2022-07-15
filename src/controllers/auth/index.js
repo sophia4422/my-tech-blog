c9165fe87f50e88f5416e39db38da41e716254e9
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-07-15 15:37:33 +0100

    login controller function

--- src/controllers/auth/index.js
+++ src/controllers/auth/index.js
@@ -1,24 +1,30 @@
 const { User } = require("../../models");
 
 const login = async (req, res) => {
-  //   try {
-  //     const { email, password } = req.body;
-  //     const user = await User.findOne({ where: { email } });
-  //     if (!user) {
-  //       console.log(
-  //         `[ERROR]: Failed to login | No user with email address of ${email}`
-  //       );
-  //       return res.status(401).json({ error: "Failed to login" });
-  //     }
-  //     const isAuthorised = await user.checkPassword(password);
-  //     if (isAuthorised) {
-  //       return res.json({ data: "hello" });
-  //     } else {
-  //       return res.status(401).json({ data: "lol" });
-  //     }
-  //   } catch (error) {
-  //     console.log(`[ERROR]: Failed to login | ${error.message}`);
-  //   }
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      console.log(
+        `[ERROR]: Failed to login | No user found with email: ${email}`
+      );
+      return res.status(500).json({ success: false });
+    }
+    const isAuthorised = await user.checkPassword(password);
+    if (isAuthorised) {
+      return res.json({ success: true });
+    } else {
+      console.log(
+        `[ERROR]: Failed to login | Incorrect password for email: ${email}`
+      );
+
+      return res.status(500).json({ success: false });
+    }
+  } catch (error) {
+    console.log(`[ERROR]: Failed to login | ${error.message}`);
+
+    return res.status(500).json({ success: false });
+  }
 };
 
 const signup = async (req, res) => {
