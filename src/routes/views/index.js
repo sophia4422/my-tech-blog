75589e74b30514954350cb4255285a3de86864e3
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-07-27 10:46:30 +0100

    auth middleware

--- src/routes/views/index.js
+++ src/routes/views/index.js
@@ -8,13 +8,14 @@ const {
   renderCreateBlogPage,
   renderBlogPage,
 } = require("../../controllers/views");
+const auth = require("../../middlewares/auth");
 
 const router = Router();
 
 router.get("/", renderHomePage);
 router.get("/login", renderLoginPage);
 router.get("/signup", renderSignUpPage);
-router.get("/dashboard", renderDashboardPage);
+router.get("/dashboard", auth, renderDashboardPage);
 router.get("/create-blog", renderCreateBlogPage);
 router.get("/blogs/:id", renderBlogPage);
 
