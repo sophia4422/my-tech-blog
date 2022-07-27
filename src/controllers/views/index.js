1c43904c8be9f857806d427c41352db8a1055039
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-07-27 12:49:00 +0100

    homepage blog

--- src/controllers/views/index.js
+++ src/controllers/views/index.js
@@ -1,8 +1,25 @@
 const path = require("path");
 
-const renderHomePage = (req, res) => {
+const { Blog, User } = require("../../models");
+
+const renderHomePage = async (req, res) => {
+  const blogsFromDb = await Blog.findAll({
+    include: {
+      model: User,
+      attributes: ["firstName", "userName"],
+    },
+    attributes: ["id", "title", "description"],
+  });
+
+  const blogs = blogsFromDb.map((blog) => {
+    return blog.get({ plain: true });
+  });
+
+  console.log(blogs);
+
   return res.render("home", {
     isLoggedIn: req.session.isLoggedIn,
+    blogs,
   });
 };
 
